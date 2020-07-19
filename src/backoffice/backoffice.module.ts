dccae82030214469798143cc1f53e3ff5139d008
danielp-oliveira <danielp.oliveira@outlook.com>
2020-07-19 10:40:01 -0300

    feat: configurando conexão e schemas do mongodb

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { VestibularController } from './controllers/vestibular.controller';
+import { MateriaSchema } from './schemas/materia.schema';
+import { QuestaoSchema } from './schemas/questao.schema';
+import { VestibularSchema } from './schemas/vestibular.schema';
 
 @Module({
-  controllers: [VestibularController]
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Vestibular', schema: VestibularSchema },
+      { name: 'Materia', schema: MateriaSchema },
+      { name: 'Questao', schema: QuestaoSchema },
+    ]),
+  ],
+  controllers: [VestibularController],
 })
 export class BackofficeModule {}
