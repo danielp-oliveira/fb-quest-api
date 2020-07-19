5971acbdac28e4e5e75fa9124dfe1eb28795cf00
danielp-oliveira <danielp.oliveira@outlook.com>
2020-07-18 21:41:56 -0300

    feat: configurando rotas no VestibularController

--- src/backoffice/controllers/vestibular.controller.ts
+++ src/backoffice/controllers/vestibular.controller.ts
@@ -1,19 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
-@Controller()
+@Controller('v1/vestibulares')
 export class VestibularController {
+  @Get()
   get() {
     return 'Obter os vestibulares';
   }
 
+  @Post()
   post() {
     return 'Criar um vestibular';
   }
 
+  @Put()
   put() {
     return 'Atualizar um vestibular';
   }
 
+  @Delete()
   delete() {
     return 'Remover um vestibular';
   }
