c7771f307c761542dc856bc218d7465cd7ce592e
danielp-oliveira <danielp.oliveira@outlook.com>
2020-07-22 00:11:38 -0300

    feat: adicionando todo o crud da materia

--- src/backoffice/controllers/materia.controller.ts
+++ src/backoffice/controllers/materia.controller.ts
@@ -6,6 +6,9 @@ import {
   HttpException,
   HttpStatus,
   Param,
+  Get,
+  Put,
+  Delete,
 } from '@nestjs/common';
 import { ValidatorInterceptor } from '@/interceptors';
 import { AddAssuntoContract, AddMateriaContract } from '@/backoffice/contracts';
@@ -21,6 +24,32 @@ export class MateriaController {
     private readonly vestibularService: VestibularSerivce,
   ) {}
 
+  @Get('admin')
+  async get() {
+    try {
+      const materias = await this.materiaService.get();
+      return MakeOkResultDto(null, materias);
+    } catch (error) {
+      throw new HttpException(
+        MakeErrorResultDto('Não foi obter as materias', error),
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Get('admin/:id')
+  async getById(@Param('id') id: string) {
+    try {
+      const materia = await this.materiaService.getById(id);
+      return MakeOkResultDto(null, materia);
+    } catch (error) {
+      throw new HttpException(
+        MakeErrorResultDto('Não foi obter a materia', error),
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
   @Post('admin')
   @UseInterceptors(new ValidatorInterceptor(new AddMateriaContract()))
   async post(@Body() model: AddMateriaDto): Promise<any> {
@@ -35,6 +64,33 @@ export class MateriaController {
     } catch (error) {
       throw new HttpException(
         MakeErrorResultDto('Não foi possível cadastrar a materia', error),
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Put('admin/:id')
+  @UseInterceptors(new ValidatorInterceptor(new AddMateriaContract()))
+  async put(@Param('id') id, @Body() model: Materia) {
+    try {
+      const materia = await this.materiaService.update(id, model);
+      return MakeOkResultDto('Materia atualizada com sucesso!', materia);
+    } catch (error) {
+      throw new HttpException(
+        MakeErrorResultDto('Não foi possível atualizar a materia', error),
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Delete('admin/:id')
+  async delete(@Param('id') id: string) {
+    try {
+      const materia = await this.materiaService.delete(id);
+      return MakeOkResultDto('Materia removida com sucesso!', materia);
+    } catch (error) {
+      throw new HttpException(
+        MakeErrorResultDto('Não foi possível remover a materia', error),
         HttpStatus.BAD_REQUEST,
       );
     }
--- src/backoffice/services/materia.service.ts
+++ src/backoffice/services/materia.service.ts
@@ -7,6 +7,11 @@ import { Materia, Assunto } from '@/backoffice/models';
 export class MateriaService {
   constructor(@InjectModel('Materia') private readonly model: Model<Materia>) {}
 
+  async get(): Promise<Materia[]> {
+    const materias = this.model.find().exec();
+    return materias;
+  }
+
   async getById(id: string): Promise<Materia> {
     const materia = this.model.findById(id).exec();
     return materia;
@@ -15,6 +20,14 @@ export class MateriaService {
   async create(data: Materia): Promise<Materia> {
     const materia = this.model(data);
     return await materia.save();
+  }
+
+  async update(id: string, data: Materia): Promise<Materia> {
+    return await this.model.findOneAndUpdate({ _id: id }, data);
+  }
+
+  async delete(id: string): Promise<Materia> {
+    return await this.model.findOneAndDelete({ _id: id });
   }
 
   async saveOrUpdateAssunto(id: string, data: Assunto): Model<Materia> {
